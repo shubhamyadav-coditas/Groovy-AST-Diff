0ea7c8b6d366922751731e33f5be4ce67d92e27d
Shubham Balasaheb Yadav <shubhamyadav@Shubhams-MacBook-Air.local>
2026-01-12 01:23:26 +0530

    removed unnecessary files

--- test-emery-remaining-samples/emery_datatable_before.groovy
+++
@@ -1,9 +0,0 @@
-// Test Emery data table operations
-def countryData = Emery.dataTable.read("COUNTRY_DATA_TABLE")
-def statusData = Emery.dataTable.read("STATUS_LOOKUP_TABLE")
-
-for (def row : countryData) {
-    println("Country: ${row.countryName}, Code: ${row.countryCode}")
-}
-
-def activeStatuses = statusData.findAll { it.isActive == true }
